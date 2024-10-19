4ccaeb012c1f778af98b9b172b8f3631ff3d1f37
Dovy <102235946+Dovy411@users.noreply.github.com>
2024-10-19 11:54:46 +0100

    Added functions: getHumanChoice and getCompChoice. Both seem to work

--- javascript.js
+++ javascript.js
@@ -1,1 +1,26 @@
-console.log("Hello world");
+
+let computerScore = 0;
+let humanScore = 0;
+
+
+const options = ["rock", "paper", "scissors"];
+
+function getComputerChoice(options) {
+    let computerChoice = options[(Math.floor(Math.random() * options.length))];
+    console.log("Computer has chosen:",computerChoice);
+    return computerChoice;
+}
+
+function getHumanChoice() {
+    let humanChoice = prompt("rock, paper, or scissors?");
+    if ((humanChoice != "rock") && (humanChoice != "paper") && (humanChoice != "scissors")) {
+        console.log("invalid input");
+        getHumanChoice();
+    } else {
+        console.log("Human has chosen:",humanChoice)
+        return humanChoice;
+    }
+}
+
+getComputerChoice(options);
+getHumanChoice();
