83c865df1765fc4da567d56b1724611440a0d6cd
Dovy4111 <102235946+Dovy411@users.noreply.github.com>
2024-11-08 10:42:01 +0000

    Not to merge. I will come back to this to clean it up

--- javascript.js
+++ javascript.js
@@ -6,77 +6,91 @@ const options = ["rock", "paper", "scissors"];
 
 function getComputerChoice(options) {
     let computerChoice = options[(Math.floor(Math.random() * options.length))];
-    console.log("Computer has chosen:",computerChoice);
+    //console.log("Computer has chosen:",computerChoice);
+    computerChoiceDisplay.textContent = ("Computer chose " + computerChoice + "!");
     return computerChoice;
 }
 
-function getHumanChoice() {
-    let humanChoice = prompt("rock, paper, or scissors?");
-    if ((humanChoice != "rock") && (humanChoice != "paper") && (humanChoice != "scissors")) {
-        console.log("invalid input");
-        getHumanChoice();
-    } else {
-        console.log("Human has chosen:",humanChoice)
-        return humanChoice;
-    }
+// function getHumanChoice() {
+//     let humanChoice = prompt("rock, paper, or scissors?");
+//     if ((humanChoice != "rock") && (humanChoice != "paper") && (humanChoice != "scissors")) {
+//         console.log("invalid input");
+//         getHumanChoice();
+//     } else {
+//         console.log("Human has chosen:",humanChoice)
+//         return humanChoice;
+//     }
+// }
+
+
+
+function displayScore() {
+    humanScoreDisplay.textContent = ("Human score: " + humanScore);
+    container.appendChild(humanScoreDisplay);
+    computerScoreDisplay.textContent = ("Computer score: " + computerScore);
+    container.appendChild(computerScoreDisplay);
 }
+
+
 
 function playRound(humanChoice,computerChoice) {
     if (humanChoice == "rock" && computerChoice == "scissors") {
-        console.log("Human wins!");
         humanScore ++;
     }
     else if (humanChoice == "rock" && computerChoice == "paper") {
-        console.log("Computer wins!");
         computerScore ++;
     }
     else if (humanChoice == "paper" && computerChoice == "rock") {
-        console.log("Human wins!");
         humanScore ++;
     }
     else if (humanChoice == "paper" && computerChoice == "scissors") {
-        console.log("Computer wins!");
         computerScore ++;
     }
     else if (humanChoice == "scissors" && computerChoice == "paper") {
-        console.log("Human wins!");
         humanScore ++;
     }
     else if (humanChoice == "scissors" && computerChoice == "rock") {
-        console.log("Computer wins!");
         computerScore ++;
     } else {
-        console.log("DRAW, no points to anyone.")
+
     }
+
+    container.appendChild(humanChoiceDisplay);
+    container.appendChild(computerChoiceDisplay);
+
+    console.log(rounds);
+
+    displayScore();
+
+    if (rounds>3) {
+        rock = document.getElementById("rock").disabled = true;
+        paper = document.getElementById("paper").disabled = true;
+        scissors = document.getElementById("scissors").disabled = true;
+
+        return displayFinalScore();
+    }
+
+    
+
+    
+    
 }
 
 
 let computerScore = 0;
 let humanScore = 0;
 
-
-function playGame(){
-
-
-
-    playRound(getHumanChoice(), getComputerChoice(options));
-    playRound(getHumanChoice(), getComputerChoice(options));
-    playRound(getHumanChoice(), getComputerChoice(options));
-    playRound(getHumanChoice(), getComputerChoice(options));
-    playRound(getHumanChoice(), getComputerChoice(options));
-
-    console.log("Human score:",humanScore);
-    console.log("Computer score:",computerScore);
+function displayFinalScore() {
 
     if (humanScore>computerScore) {
-        console.log("Human wins the game!");
+        results.textContent = ("Human wins!");
     }
     else if (humanScore<computerScore) {
-        console.log("Computer wins the game!");
+        results.textContent = ("Computer wins!");
     } else {
-        console.log("DRAW");
+        results.textContent = ("DRAW, we all win!");
     }
+
+    container.appendChild(results);
+    
 }
-
-playGame();
-
